155d332c4d81236ec6aa63e24686e0e843c3aec9
Mathieu Boespflug <mboes@tweag.net>
2014-12-07 13:15:26 +0100

    Add tests for pattern name splices.

--- tests/test.hs
+++ tests/test.hs
@@ -1,5 +1,6 @@
 import Language.Haskell.Exts.QQ
 
+import qualified Language.Haskell.Exts as Hs
 import Test.Hspec
 
 main :: IO ()
@@ -9,6 +10,14 @@ main = hspec $ do
         (\x -> [hs| $x |]) [hs| 10 |] `shouldBe` [hs| 10 |]
       it "Substitutes for splices in composite terms" $ do
         (\x -> [hs| $x + $x |]) [hs| 10 |] `shouldBe` [hs| 10 + 10 |]
+
+    describe "Pattern name splices" $ do
+      it "Substitutes for splices in atomic patterns" $ do
+        (\x -> [hs| case 10 of ((x)) -> y |]) (Hs.name "y")
+            `shouldBe` [hs| case 10 of y -> y |]
+      it "Substitutes for splices in composite patterns" $ do
+        (\x -> [hs| let (Just ((x)), x) = (Just 10, 10) in y |]) (Hs.name "y")
+            `shouldBe` [hs| let (Just y, x) = (Just 10, 10) in y |]
 
     describe "type splices" $ do
       it "Substitutes for splices in atomic types" $ do
